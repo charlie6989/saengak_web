183b8d5cffb3aae44f849abd70b2b3159ba3f033
Charlie Liu <charlieliu@Charlies-MacBook-Pro.local>
2026-01-07 20:57:28 +0800

    test: add Header to Home

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,10 +1,15 @@
+import Header from '../../components/feature/Header';
+
 export default function Home() {
-  console.log('🏠 ULTRA SIMPLE HOME');
+  console.log('🏠 Testing with Header');
 
   return (
-    <div style={{ padding: 50, fontSize: 30, background: 'lime', color: 'black', minHeight: '100vh' }}>
-      <h1>ULTRA SIMPLE HOME PAGE</h1>
-      <p>If you see this green page, we've isolated the problem!</p>
+    <div style={{ minHeight: '100vh', background: 'white' }}>
+      <Header />
+      <div style={{ padding: 50, fontSize: 30, background: 'lime', color: 'black' }}>
+        <h1>Home with Header</h1>
+        <p>If you see this, Header is OK. Will test Footer next.</p>
+      </div>
     </div>
   );
 }
